c2dcb1f7af7c215e1fa711335a3eccfcb078c9bf
jesusalejandro.valladares.sol <jesusalejandro.valladares.sol@bbva.com>
2021-08-29 00:29:03 -0500

    implemented BFS

---
+++ src/main/java/mx/chux/cs/alg/graph/BreadthFirstSearch.java
@@ -0,0 +1,106 @@
+package mx.chux.cs.alg.graph;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.function.BiFunction;
+
+import mx.chux.cs.ds.graph.Graph;
+import mx.chux.cs.ds.graph.Node;
+
+public class BreadthFirstSearch<T extends Comparable<T>> implements BiFunction<T, T, Collection<T>> {
+
+    final Graph<T> graph;
+    
+    public BreadthFirstSearch(final Graph<T> graph ) {
+        this.graph = graph;
+    }
+    
+    @Override
+    public Collection<T> apply(T first, T last) {
+        
+        final Node<T> firstNode = this.graph.getNode(first);
+        final Node<T> lastNode = this.graph.getNode(last);
+        
+        if( (firstNode == null) || (lastNode == null) ) {
+            return Collections.emptyList();
+        }
+        
+        final Collection<Node<T>> path = bfs(firstNode, lastNode);
+        
+        return extractPath(path);
+    }
+    
+    private Collection<Node<T>> bfs(Node<T> first, Node<T> last) {
+        
+        final Queue<Node<T>> q = new LinkedList<>();
+        final Map<Node<T>, Node<T>> parents = new HashMap<>(this.graph.sizeOfNodes());
+        
+        final T value = last.get();
+        
+        q.add(first);
+        
+        while( !q.isEmpty() ) {
+            
+            final Node<T> node = q.remove();
+            
+            if( !node.hasBeenVisited() ) {
+
+                node.setVisited(true);
+                
+                if( node.get().equals(value) ) {
+                    return backtrace(parents, first, last);
+                }
+                
+                final Collection<Node<T>> adjecents = this.graph.getAdjacentNodes(node);
+                
+                for( Node<T> n : adjecents ) {
+                    if( !q.contains(n) ) {
+                        parents.put(n, node);
+                        q.add(n);
+                    }
+                }
+                
+            }
+            
+        }
+        
+        return Collections.emptyList();
+    }
+    
+    private Collection<Node<T>> backtrace(Map<Node<T>, Node<T>> parents, Node<T> first, Node<T> last) {
+        
+        final List<Node<T>> path = new ArrayList<>(parents.size());
+        
+        path.add(last);
+        
+        Node<T> current = last;
+        Node<T> parent = parents.get(current);
+        
+        while( !parent.equals(first) ) {
+            path.add(parent);
+            current = parent;
+            parent = parents.get(parent);
+        }
+         
+        path.add(parent);
+        
+        Collections.reverse(path);
+        
+        return path;
+    }
+    
+    private Collection<T> extractPath(Collection<Node<T>> path) {
+        final List<T> extractedPath = new ArrayList<>(path.size());
+        for( Node<T> node : path ) {
+            extractedPath.add(node.get());
+        }
+        return extractedPath;
+    }
+
+}
---
+++ src/test/java/mx/chux/cs/alg/graph/BreadthFirstSearchTest.java
@@ -0,0 +1,67 @@
+package mx.chux.cs.alg.graph;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import mx.chux.cs.ds.graph.Graph;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class BreadthFirstSearchTest {
+
+    Graph<String> graph;
+    BreadthFirstSearch<String> bfs;
+    
+    @Before
+    public void initializeGraph() {
+        Map<String, Collection<String>> data = new HashMap<>();
+        
+        data.put("a", Arrays.asList("b", "c", "e"));
+        data.put("b", Arrays.asList("c"));
+        data.put("c", Arrays.asList("d"));
+        data.put("o", Collections.emptyList());
+        
+        this.graph = new Graph<>(data);
+        this.bfs = new BreadthFirstSearch<>(this.graph);
+    }
+    
+    @Test
+    public void pathMustExistsTest() {
+        Collection<String> path = this.bfs.apply("a", "d");
+        assertThat(path).isNotEmpty()
+        .isEqualTo(Arrays.asList("a", "c", "d"));
+    }
+    
+    @Test
+    public void pathMustNotExistTest() {
+        assertThat(this.bfs.apply("b", "o")).isEmpty();
+    }
+    
+    @Test
+    public void transitivePathExistTest() {
+        assertThat(this.bfs.apply("b", "e")).isNotEmpty()
+        .isEqualTo(Arrays.asList("b", "c", "a", "e"));
+    }
+    
+    @Test
+    public void illegalLastNodeTest() {
+        assertThat(this.bfs.apply("a", "x")).isEmpty();
+    }
+    
+    @Test
+    public void illegalFirstNodeTest() {
+        assertThat(this.bfs.apply("x", "a")).isEmpty();
+    }
+    
+    @Test
+    public void illegalFirstAndLastNodesTest() {
+        assertThat(this.bfs.apply("x", "x")).isEmpty();
+    }
+    
+}
